96a843be02e6f8d26092b85d0c01277295755c88
Rahat Morshed Nabil <imrmnabil@gmail.com>
2022-07-23 17:25:20 +0600

    Done almost visual part

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const pokeData = require('./data/pokedex.json');
+const typeData = require('./data/types.json');
 const app = express();
 const port = 3000;
 
@@ -9,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('dex')
 });
 
 app.listen(port, () => {
