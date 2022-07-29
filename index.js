6abe841c18d1e6069286f7dd97a9050666997932
Rahat Morshed Nabil <imrmnabil@gmail.com>
2022-07-29 12:14:26 +0600

    Resolve heruku port issue in index,js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const pokeData = require('./data/pokedex.json');
 const typeData = require('./data/types.json');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ||5000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
